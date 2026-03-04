6456959a4a99fd197f79283c6359fc0cfea9c6fc
Komal mahendrakar <komalmahendrakar07@gmail.com>
2026-03-04 15:37:06 +0000

    add this as primary number: 7338604777

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,9 +3,9 @@
  */
 
 export const CONTACT_INFO = {
-  phone: "+918022223456",
-  displayPhone: "+91 802222 3456",
-  directPhone: "+91 98765 43210",
+  phone: "+917338604777",
+  displayPhone: "+91 73386 04777",
+  directPhone: "+91 73386 04777",
   email: "contact@drpoorneshgowda.com",
   address: {
     hospital: "The Bangalore Hospital",
